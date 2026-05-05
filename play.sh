62b348e211e050a2ffbd1a842b6ff6ba8a7e8dfc
Porshen Lai <porshenlai@gmail.com>
2026-05-05 22:37:44 +0800

    fix play.sh for POSIX

--- play.sh
+++ play.sh
@@ -4,12 +4,15 @@ ROOT=$(dirname $0)
 VENV=${ROOT}/venv
 PY=${VENV}/bin/python3
 
-test -f "${PY}" || PY="${VENV}/Scripts/python.exe"
 test -f "${PY}" || {
 	THONNY=/d/apps/thonny/python.exe
 	if test -f "${THONNY}"; then
 		echo "Install from thonny"
 		${THONNY} -m venv ${VENV}
+		${PY} -m pip install --upgrade pip aiohttp aiohttp_cors aiofiles
+	else 
+		echo "Install on POSIX"
+		python3 -m venv ${VENV}
 		${PY} -m pip install --upgrade pip aiohttp aiohttp_cors aiofiles
 	fi
 }
